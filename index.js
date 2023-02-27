06e755de103ad590338f2daeb267068b5712c5fd
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-02-27 15:24:24 -0500

    add --json flag

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const ConfigParser = require('@webantic/nginx-config-parser')
 const parser = new ConfigParser()
 const fs = require('fs')
 
-const config = parser.readConfigFile('./test.conf', { parseIncludes: false })
+const config = parser.readConfigFile('/etc/nginx/sites-available/default', { parseIncludes: false })
 let out = []
 
 config.server.filter(s => s['location /']).forEach(s => out.push({
@@ -10,8 +10,11 @@ config.server.filter(s => s['location /']).forEach(s => out.push({
     proxy_pass: s['location /'].proxy_pass
 }))
 
-fs.writeFileSync("./out.json", JSON.stringify(out, null, 2))
-
-for(let server of out) {
-    console.log(`Domain: ${server.server_name} | Location: ${server.proxy_pass}`)
+if(process.argv[2] == "--json" || process.argv[2] == "-j") {
+	console.log(JSON.stringify(out));
+} else {
+    //fs.writeFileSync("./out.json", JSON.stringify(out, null, 2))
+    for(let server of out) {
+        console.log(`Domain: ${server.server_name} | Location: ${server.proxy_pass}`)
+    }
 }
